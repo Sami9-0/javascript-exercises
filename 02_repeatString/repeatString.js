970164547c2d99ffc465fe7dfeb6a19343382abd
Sami9-0 <samsontsegenate@gmail.com>
2022-11-29 21:56:58 +0300

    Completed first and secon assignment

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(text, num) {
+    let repeat = '';
+    if (num < 0)
+    {
+        return 'ERROR';
+    }
+    for (let i = 0; i < num; i++)
+    {
+        repeat += text;
+    }
+    return repeat;
 };
 
 // Do not edit below this line
