a15c99ed36f7b2241285661c7cbc0186892e96b4
Sami9-0 <samsontsegenate@gmail.com>
2022-11-29 22:09:07 +0300

    Finished exersice 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(text) {
+    let myArray = text.split("");
+    let reverse = '';
+    for (let i = text.length - 1; i >= 0; i--)
+    {
+        reverse += myArray[i];
+    }
+    return reverse;
 };
 
 // Do not edit below this line
