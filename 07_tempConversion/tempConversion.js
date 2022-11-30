ce433abd169eedaa68b4fd8c88c21a2d510d4e80
Sami9-0 <samsontsegenate@gmail.com>
2022-11-30 21:43:30 +0300

    Changed varible declartion from let to const

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,11 @@
 const convertToCelsius = function(fahrenheit) {
-  let celsius = (fahrenheit - 32) * (5 / 9);
+  const celsius = (fahrenheit - 32) * (5 / 9);
 
   return Math.round(celsius * 10) / 10;
 };
 
 const convertToFahrenheit = function(celsius) {
-  let fahrenheit = (celsius * (9 / 5) + 32);
+  const fahrenheit = (celsius * (9 / 5) + 32);
   return Math.round(fahrenheit * 10) / 10;
 };
 
