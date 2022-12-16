dea1bf0cad650efd6b363fd79178da69777ced38
Sami9-0 <samsontsegenate@gmail.com>
2022-12-16 13:53:30 +0300

    Finished exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,21 @@
-const palindromes = function () {
-
+const palindromes = function (theString) {
+    theString = (theString.replace(/[^a-zA-Z0-9]/g, ''));
+    theString = theString.toLowerCase();
+    const splitString = theString.split('');
+    let reversedString = '';
+    for (let i = splitString.length - 1; i >=0; i--)
+    {
+        reversedString += splitString[i];
+    }
+    reversedString = reversedString.split('');
+    for(let i = 0; i <= splitString.length-1; i++)
+    {
+        if (reversedString[i] != splitString[i])
+        {
+            return false;
+        }
+    }
+    return true;
 };
 
 // Do not edit below this line
