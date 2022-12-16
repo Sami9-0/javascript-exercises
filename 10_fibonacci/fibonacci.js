b4503b711f6fb09ecd3cfef78edab3d871da43f9
Sami9-0 <samsontsegenate@gmail.com>
2022-12-16 14:19:38 +0300

    Finished exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,26 @@
-const fibonacci = function() {
+const fibonacci = function(index) {
+    if (!index.isInteger)
+    {
+        index = parseInt(index);
+    }
+    let previousNum = 1;
+    let currentNum = 1;
+    if (index === 1 || index === 2)
+    {
+        return 1;
+    }
+    else if (index < 0)
+    {
+        return 'OOPS';
+    }
 
+    for (let i = 2; i < index; i++)
+    {
+        let saved = currentNum;
+        currentNum = currentNum + previousNum;
+        previousNum = saved;
+    }
+    return currentNum;
 };
 
 // Do not edit below this line
