1b87aa3e1a7504fb6cc38002b7782c3e621a7607
Sami9-0 <samsontsegenate@gmail.com>
2022-12-16 12:26:26 +0300

    Finished exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+	const index = numbers.length;
+  let sum = 0;
+  for(let i = 0; i < index; i++)
+  {
+    sum += numbers[i];
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(numbers) {
+  const index = numbers.length;
+  let product = 1;
+  for(let i = 0; i < index; i++)
+  {
+    product *= numbers[i];
+  }
+  return product;
 };
 
-const power = function() {
-	
+const power = function(x, y) {
+	return x**y;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+  let fac = 1;
+	return (number === 0 || number === 1) ? 1 : (number * factorial(number - 1));
 };
 
 // Do not edit below this line
